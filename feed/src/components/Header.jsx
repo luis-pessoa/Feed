728a074246a3ecbd888599d352aeeacaa10b7e4e
luis-pessoa <gustavo@PRPPG-1037096.intranet.ufba.br>
2025-04-23 10:27:02 -0300

    10:26 23-04-2025

---
+++ feed/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header() {
+  return (
+     <div className='bg-[#202024] h-[100px] flex items-center justify-center'>
+        <div className='flex gap-2 items-center'>
+            <img src="./Ignite simbol.png" alt="" />
+            <p className='text-[#E1E1E6] text-[25.59px] font-bold'>Ignite Feed</p>
+        </div>
+          
+     </div>
+  )
+}
+
+export default Header
